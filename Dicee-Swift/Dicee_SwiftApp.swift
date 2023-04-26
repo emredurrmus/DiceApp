fd5a9b12b908a76f4e4b3c368e717443aac4ac69
emredurrmus <emredurmus0@hotmail.com>
2023-04-26 23:33:36 +0300

    Initial Commit

---
+++ Dicee-Swift/Dicee_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftApp.swift
+//  Dicee-Swift
+//
+//  Created by Emre Durmuş on 26.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
